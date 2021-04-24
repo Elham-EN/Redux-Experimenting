52bb2ab545fd98b56ebdb577c86f23412f52901a
Elham-EN <ilhamrezaie@gmail.com>
2021-04-24 14:51:39 +1000

    Made my application state persisted using redux-persister

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { configureStore } from "./store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const store = configureStore();
+const persistor = persistStore(store);
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store.js
+++ src/store.js
@@ -1,10 +1,27 @@
 import { createStore, combineReducers } from "redux";
+//Need these objects to make our redux store persist
+import { persistReducer } from "redux-persist";
+//Default local storage to web
+import storage from "redux-persist/lib/storage";
+//tell redux persist how to reconcile/integrate the initial and stored
+//states of our application, as in how deep should it go when doing so
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { todos } from "./todos/reducers";
 
 //put all reducer
 const reducers = { todos };
 
+//persistConfig object tells Redux persist, how to save and where to
+//store our application data
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
 //put reducers in a form tat we can pass to the createStore
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
